0af4803e73d1e2fd95aab83a34db4b443cdaa568
mm <mm@mm.com>
2024-11-17 15:18:38 +0800

    -- click

---
+++ docs/cache_image/README.md
@@ -0,0 +1,4 @@
+### 结合[ScrollAwareImageProvider]和[CacheNetworkImage] 组合成的可缓存的图片组件
+- 适用于列表中的图片 
+- 适用于需要签名得到图片地址的图片
+- 地址[https://github.com/knight-mm/flutter_tools/tree/beta/resources/cache_image]
---
+++ resources/click_debounce/lib/click_debounce.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class ClickDebounce extends StatefulWidget {
+  const ClickDebounce(
+      {super.key, required this.count, required this.originStatus, this.onTap});
+
+  final int count;
+
+  final bool originStatus;
+
+  final Function(int, bool)? onTap;
+
+  @override
+  State<ClickDebounce> createState() => _ClickDebounceState();
+}
+
+class _ClickDebounceState extends State<ClickDebounce> {
+  int count = 0;
+  bool status = false;
+
+  Timer? debounce;
+
+  @override
+  void initState() {
+    super.initState();
+    count = widget.count;
+    status = widget.originStatus;
+  }
+
+  @override
+  void didUpdateWidget(covariant ClickDebounce oldWidget) {
+    count = widget.count;
+    status = widget.originStatus;
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    debounce?.cancel();
+    super.dispose();
+  }
+
+  void onTapItem() {
+    status = !status;
+    count += status ? 1 : -1;
+    if (mounted) setState(() {});
+    if (widget.onTap != null) {
+      if (debounce?.isActive ?? false) debounce?.cancel();
+      debounce = Timer(const Duration(milliseconds: 500), () {
+        widget.onTap!(count, status);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTapItem,
+      child: Container(
+        height: 100,
+        width: 100,
+        color: status ? Colors.green : Colors.red,
+        child: Text("$count"),
+      ),
+    );
+  }
+}
